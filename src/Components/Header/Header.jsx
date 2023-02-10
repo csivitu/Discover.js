6d3a61dca9d40ad9e152e3216fa5518ab99d15a9
adityak840 <khandelwal.aditya840@gmail.com>
2023-02-10 22:15:46 +0530

    fix: Header and Timeline

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import csi from './../Images/csi.svg'
+import hamburger_menu from './../Images/hamburger_menu.svg'
 
 const Header = () => {
+  function toggle(){
+    if (document.getElementById("menu").className == 'text-white text-2xl text-yellow lg:hidden translate-x-0'){
+      document.getElementById("menu").className = 'text-white text-2xl text-yellow lg:hidden -translate-x-full';
+      console.log("hha");
+    }
+    else{
+      document.getElementById("menu").className = 'text-white text-2xl text-yellow lg:hidden translate-x-0';
+      console.log("hje")
+    }
+  }
   return (
     <section>
         <header>
-            <div className='container mx-auto px-20 py-10 md:justify-end justify-center flex'>
+            <div className='container mx-auto lg:px-20 px-5 py-16 justify-between flex'>
+                <div>
+                  <img src={csi} className="w-100% pt-2" alt="logo" />
+                </div>
+                
                 <ul className='flex md:gap-x-24 gap-x-8 text-yellow text-xl font-body'>
-                    <li><a className ='hover:text-sky-50 duration-500' href="#home">Home</a></li>
-                    <li><a className ='hover:text-sky-50 duration-500' href="#timeline">Timeline</a></li>
-                    {/* <li><a className ='hover:text-sky-50 duration-500' href="#ContactUs">Contact</a></li>
-                    <li><a className ='hover:text-sky-50 duration-500' href="https://vtop.vit.ac.in/">Register Now</a></li> */}
+                    <li><a className ='lg:visible invisible hover:text-sky-50 duration-500' href="#home">Home</a></li>
+                    <li><a className ='lg:visible invisible hover:text-sky-50 duration-500' href="#timeline">Timeline</a></li>
                 </ul>
+                <img id = "icon" onClick={() => {
+                  toggle()
+                }} src={hamburger_menu} className="lg:hidden mb-2" alt="logo" />
+            </div>
+            <div id = "menu" className='text-white text-2xl text-yellow lg:hidden -translate-x-full'>
+              <ul className='flex flex-col items-center text-yellow'>
+                <li><a className ='hover:text-sky-50 duration-500' href="#home">Home</a></li>
+                <li><a className ='hover:text-sky-50 duration-500' href="#timeline">Timeline</a></li>
+              </ul>
             </div>
         </header>
     </section>
   )
+  
 }
 
 export default Header
+
+
+
